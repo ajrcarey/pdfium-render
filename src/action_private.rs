9df8dcb25c61d53fd52519c63ffd442b81adf4c9
Alastair Carey <alastair@alastaircarey.com>
2024-06-13 21:47:31 +0100

    Resolves #142

--- src/action_private.rs
+++ src/action_private.rs
@@ -14,6 +14,7 @@ pub(crate) mod internal {
     /// Internal crate-specific functionality common to all [PdfAction] actions.
     pub(crate) trait PdfActionPrivate<'a>: PdfActionCommon<'a> {
         /// Returns the internal `FPDF_ACTION` handle for this [PdfAction].
+        #[allow(dead_code)] // TODO: AJRC - 13/6/24 - remove once handle() function is in use.
         fn handle(&self) -> &FPDF_ACTION;
 
         /// Returns the [PdfiumLibraryBindings] used by this [PdfAction].
